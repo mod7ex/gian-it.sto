467545800e0e7f2c2a6415a5a66a33bf9aa51af7
Enakaev Ildar <ienakaev@ya.ru>
2021-10-14 14:44:28 +0300

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory }  from 'vue-router';
+import { createRouter, createWebHashHistory }  from 'vue-router';
 import App from './App.vue'
 import routes from './routes.js';
 
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes, // short for `routes: routes`
 });
 

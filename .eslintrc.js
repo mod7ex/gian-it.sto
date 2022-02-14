2c8e0b491217778f418c2f1a9e1858f907e7c809
Julia Rizhenkova <julixandrina@gmail.com>
2022-02-14 21:43:27 +0100

    Additional configuration of eslintrc for correct work in the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb",
-        //"eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    // "eslint:recommended",
+    'plugin:vue/essential',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'object-curly-newline': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'max-len': 'off',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+  },
+
 };

add9e28bd222d63bb9f7df1176d841acc5793596
Julia Rizhenkova <julixandrina@gmail.com>
2022-02-17 20:13:48 +0100

    Добавлена валидация для страницы забыли пароль

--- src/components/Pages/Login/ForgotPassword.vue
+++ src/components/Pages/Login/ForgotPassword.vue
@@ -1,6 +1,6 @@
 <script setup>
-// import { useRouter } from 'vue-router';
 import { ref } from 'vue';
+import Validator from 'Validator';
 import { ArrowNarrowLeftIcon } from '@heroicons/vue/outline';
 import Button from '@/UI/Button.vue';
 import Input from '@/UI/Input.vue';
@@ -8,30 +8,65 @@ import Link from '@/UI/Link.vue';
 import LoginLayout from '@/Layout/Login.vue';
 import useApi from '~/composables/useApi.js';
 
-// const router = useRouter();
 const { axiosInstance } = useApi();
+const refreshPageTitle = ref('Забыли пароль?');
 const email = ref('');
 const success = ref(false);
 const successMessage = ref('');
 const error = ref(false);
 const errorMessage = ref('');
 const loading = ref(false);
+const validationErrors = ref({
+  email: '',
+});
 
-const refresh = async () => {
+function cleanErrors() {
+  validationErrors.value.email = '';
   error.value = false;
   errorMessage.value = '';
+}
+
+function makeValidator() {
+  const validateData = {
+    email: email.value,
+  };
+
+  const validateRules = {
+    email: 'required|email',
+  };
+
+  const validateMessages = {
+    'email.email': 'Укажите верный email',
+    'email.required': 'Укажите email',
+  };
+
+  return Validator.make(validateData, validateRules, validateMessages);
+}
+
+const refresh = async () => {
+  const validator = makeValidator();
+
+  if (validator.fails()) {
+    const errors = validator.getErrors();
+    validationErrors.value = {
+      email: (errors.email) ? errors.email[0] : '',
+    };
+    return;
+  }
+
+  cleanErrors();
+
+  loading.value = true;
   try {
-    console.log(email.value);
     const res = await axiosInstance.post('auth/password/email', {
       email: email.value,
     });
-    console.log(res.data);
     success.value = true;
-    successMessage.value = res.data.message;
+    successMessage.value = email.value;
+    refreshPageTitle.value = res.data.message;
   } catch (e) {
-    console.log(e.response.data);
     error.value = true;
-    errorMessage.value = e.response.data.message;
+    errorMessage.value = (e.response) ? e.response.data.message : 'Undefined (network?) error';
   } finally {
     loading.value = false;
   }
@@ -39,15 +74,17 @@ const refresh = async () => {
 </script>
 
 <template>
-    <LoginLayout title="Забыли пароль?">
+    <LoginLayout :title="refreshPageTitle">
       <div class="mt-8">
         <div class="mt-6">
-          <p v-if="success" class="text-green-700 font-bold mb-6">{{ successMessage }}</p>
-          <p v-if="error" class="text-red-500  text-center font-bold mb-6">{{ errorMessage }}</p>
+          <p v-if="success" class="text-green-700 mb-6">{{ email }}</p>
+          <p v-if="error" class="text-red-500 text-sm mb-6">{{ errorMessage }}</p>
           <form action="#" method="POST" class="space-y-6">
-            <Input label="E-mail" type="email" help="Введите ваш email, на него придёт ссылка для сброса пароля" v-model="email" />
+            <Input label="E-mail" type="email" help="Введите ваш email, на него придёт ссылка для сброса пароля"
+            v-model="email" v-if="!success" :error="validationErrors.email" />
 
-            <Button color="blue" class="w-full justify-center" @click="refresh" :disabled="loading" :class="{ 'cursor-not-allowed': loading, 'opacity-60': loading }">
+            <Button color="blue" class="w-full justify-center" @click="refresh" :disabled="loading"
+            :class="{ 'cursor-not-allowed': loading, 'opacity-60': loading }" v-if="!success">
               <span v-if="!loading">Отправить</span>
               <div
               v-if="loading"

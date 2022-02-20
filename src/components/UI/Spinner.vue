ffc9e9a4f543c4c8dbffe60ec7a095b6a64a2a47
Julia Rizhenkova <julixandrina@gmail.com>
2022-02-20 19:36:42 +0100

    Небольшая модификация спиннера

--- src/components/UI/Spinner.vue
+++ src/components/UI/Spinner.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <div class="spinner-border animate-spin inline-block w-8 h-8 border-b-2 rounded-full" role="status"></div>
-    <slot></slot>
+   <div class="w-8 h-8 border-4 border-blue-400 borderTopColorTransparent border-solid rounded-full animate-spin"></div>
+   <slot></slot>
   </div>
-  </template>
+</template>
 
 <style scoped>
-
+.borderTopColorTransparent {
+  border-top-color: transparent;
+}
 </style>

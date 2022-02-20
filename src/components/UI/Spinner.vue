18b2a646d80faa21a643e87c6bff52364aeee049
Julia Rizhenkova <julixandrina@gmail.com>
2022-02-20 19:13:31 +0100

    Вынесение spinner для кнопок в отдельный компонент

---
+++ src/components/UI/Spinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="spinner-border animate-spin inline-block w-8 h-8 border-b-2 rounded-full" role="status"></div>
+    <slot></slot>
+  </div>
+  </template>
+
+<style scoped>
+
+</style>

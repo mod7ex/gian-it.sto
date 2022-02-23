c53687d0602b9aa99aba18597f48146c887505d2
Julia Rizhenkova <julixandrina@gmail.com>
2022-02-23 21:00:46 +0100

    Изменения для переиспользования правил в разных фалах

--- src/validationsRules/refreshPassword.js
+++ src/validationsRules/refreshPassword.js
@@ -1,22 +1,19 @@
 import { ref, computed } from 'vue';
 import { required, sameAs, minLength, helpers } from '@vuelidate/validators';
 
-const fieldLength = ref(8);
-let form;
+export default function refreshPasswordValidationsRules(formModel) {
+  const fieldLength = ref(8);
 
-const rules = computed(() => ({
-  password: {
-    required: helpers.withMessage('Укажите новый пароль', required),
-    minLength: helpers.withMessage(`Пароль должен содержать не менее ${fieldLength.value} символов`, minLength(fieldLength)),
-  },
-  confirmPassword: {
-    required: helpers.withMessage('Укажите пароль ещё раз', required),
-    sameAs: helpers.withMessage('Пароли должны совпадать', sameAs(form.password)),
-  },
-}));
-
-export default function useRefreshPasswordValidationsRules(formModel) {
-  form = formModel;
+  const rules = computed(() => ({
+    password: {
+      required: helpers.withMessage('Укажите новый пароль', required),
+      minLength: helpers.withMessage(`Пароль должен содержать не менее ${fieldLength.value} символов`, minLength(fieldLength)),
+    },
+    confirmPassword: {
+      required: helpers.withMessage('Укажите пароль ещё раз', required),
+      sameAs: helpers.withMessage('Пароли должны совпадать', sameAs(formModel.password)),
+    },
+  }));
 
   return {
     rules,

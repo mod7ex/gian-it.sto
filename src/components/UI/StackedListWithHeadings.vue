c84dd38a7b2e74222064d478428fcc4c600a309d
Mourad EL CADI <mouradface1998@gmail.com>
2022-05-23 11:42:03 +0100

    used event delegation

--- src/components/UI/StackedListWithHeadings.vue
+++ src/components/UI/StackedListWithHeadings.vue
@@ -10,6 +10,15 @@ const props = defineProps({
     required: false,
   },
 });
+
+const emit = defineEmits(['select']);
+
+const pick = (e) => {
+  const id = e.target?.id;
+  if (Number.isNaN(id)) return;
+  emit('select', id);
+};
+
 </script>
 
 <template>
@@ -19,16 +28,20 @@ const props = defineProps({
         <h3>{{ letter }}</h3>
       </div>
 
-      <ul role="list" class="relative z-0 divide-y divide-gray-200">
-        <li v-for="item in props.items[letter]" :key="item.id" :class="[`bg-${selected == item.id ? 'gray-200 ring-indigo-300 ring-inset ring-1' : 'white'}`, `hover:bg-gray-${selected == item.id ? '200' : '100'}`]">
+      <ul role="list" class="relative z-0 divide-y divide-gray-200" @click="($e)=>pick($e)">
+        <li
+          v-for="item in props.items[letter]"
+          :key="item.id"
+          :class="[`bg-${selected == item.id ? 'gray-200 ring-indigo-300 ring-inset ring-1' : 'white'}`, `hover:bg-gray-${selected == item.id ? '200' : '100'}`]"
+        >
           <div class="relative px-6 py-5 flex items-center space-x-3 ">
             <div class="flex-shrink-0" v-if="item.image">
               <img class="h-10 w-10 rounded-full" :src="item.image" alt="" />
             </div>
 
             <div class="flex-1 min-w-0">
-              <button @click="$emit('select', item.id)" class="focus:outline-none text-left">
-                <span class="absolute inset-0" aria-hidden="true" />
+              <button class="focus:outline-none text-left">
+                <span class="absolute inset-0" aria-hidden="true" :id="item.id"/>
                 <p class="text-sm font-medium text-gray-900" v-if="item.title">
                   {{ item.title }}
                 </p>

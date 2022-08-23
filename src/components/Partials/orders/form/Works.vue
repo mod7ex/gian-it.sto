b2cf8c0eebdd26fa71c02c7b362e86b76fa7dd82
Pradd <Pradd.by@gmail.com>
2022-08-23 14:59:34 +0300

    bitrix24#471: hide button Create Work

--- src/components/Partials/orders/form/Works.vue
+++ src/components/Partials/orders/form/Works.vue
@@ -4,8 +4,11 @@ import Button from '@/UI/Button.vue';
 import service from '~/services/orders/work';
 import Table from '@/Partials/orders/items/Works.vue';
 import useSuspense from '~/composables/useSuspense';
+import { userHasPermission } from '~/lib/permissions.js';
 
 const SuspenseArea = useSuspense(Table);
+
+const hasCRUDworks = userHasPermission('crud works');
 
 const { render } = service();
 
@@ -14,7 +17,11 @@ const { render } = service();
 <template>
     <div>
       <div class="mb-5">
-        <Button color="blue" @click="() => render()">
+        <Button
+          v-if="hasCRUDworks"
+          color="blue"
+          @click="() => render()"
+        >
           <ChipIcon class="w-5 h-5 mr-1"/>Добавить работу
         </Button>
       </div>

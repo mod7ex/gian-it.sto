5a5ba975cf215f0c2ec0e4ad4fea81fe49ad87e0
Mourad <mouradface1998@gmail.com>
2022-09-09 18:43:16 +0100

    devices

--- src/components/Pages/Work/RawKanban.vue
+++ src/components/Pages/Work/RawKanban.vue
@@ -15,8 +15,10 @@ watch(theSelectedFunnel, fillColumns);
 
 await atMounted();
 
-const canDragAndDrop = (userHasPermission('read own tasks') && userHasPermission('update status tasks'))
-  || (userHasPermission('update tasks') && !userHasPermission('read own tasks'))
+const READ_ALL_TASKS_IN_KANBAN = 'read own tasks'
+
+const canDragAndDrop = (userHasPermission(READ_ALL_TASKS_IN_KANBAN) && userHasPermission('update status tasks'))
+  || (userHasPermission('update tasks') && !userHasPermission(READ_ALL_TASKS_IN_KANBAN))
 
 /*
 

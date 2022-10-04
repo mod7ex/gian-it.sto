84427f7a88fd08a77048d79525ee9954a15d4b4e
Mourad <mouradface1998@gmail.com>
2022-10-04 13:33:27 +0100

    Fixes

--- src/components/Partials/orders/form/Raw/StorageRequest.vue
+++ src/components/Partials/orders/form/Raw/StorageRequest.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { watch } from 'vue';
+import { computed, watch } from 'vue';
 import Input from '@/UI/Input.vue';
 import Select from '@/UI/Select.vue';
 import TextArea from '@/UI/TextArea.vue';
@@ -13,7 +13,7 @@ import { tasksColorMap } from '~/helpers';
 
 const { items, atMounted, products_request } = service();
 const { current } = departmentStore;
-const { fill, options } = store;
+const { fill, options, state: ss } = store;
 const { load: loadStorages, options: storageOptions } = storageStore;
 const { loadStatuses, state } = storageRequestsStore;
 
@@ -22,11 +22,14 @@ let statusOptions;
 await Promise.all([loadStorages({ department_id: current.value }), loadStatuses(), atMounted()]).then(()=>{
   statusOptions = state.statuses.map((value) => ({ label: tasksColorMap[value].label, value }));
 })
+
 // await Promise.all([load({ storage_id: products_request.storage_id }), loadStorages({ department_id: current.value }), atMounted()]);
 
 // const statusOptions = Object.entries(tasksColorMap).map(([value, { label }]) => ({ label, value }));
 
 watch(() => products_request.storage_id, async (storage_id) => { await fill({ storage_id }); }, {immediate: true});
+
+const selectedProduct = computed(() => ss.raw.find(({id}) => products_request.product_id == id))
 
 </script>
 
@@ -37,7 +40,7 @@ watch(() => products_request.storage_id, async (storage_id) => { await fill({ st
           :options="storageOptions"
           :required="true"
           v-model="products_request.storage_id"
-          class="m-3 sm:col-span-6 col-span-12"
+          class="mb-3 sm:col-span-6 col-span-12"
         />
 
         <sto-select
@@ -47,16 +50,24 @@ watch(() => products_request.storage_id, async (storage_id) => { await fill({ st
           :options="options"
           :required="true"
           v-model="products_request.product_id"
-          class="m-3 sm:col-span-6 col-span-12"
+          class="mb-3 sm:col-span-6 col-span-12"
           @bottom-touched="() => fill({ client_id: fields.client_id })"
         />
+
+        <div class="mb-6">
+          <span>цена</span>
+          <span class="rounded shadow p-2 block" >
+            {{ !selectedProduct?.output_sum ? '_' : `${selectedProduct?.output_sum} ₽` }}
+          </span>
+        </div> 
+
 
         <Input
           label="Количество"
           type="number"
           :required="true"
           v-model="products_request.count"
-          class="m-3 sm:col-span-6 col-span-12"
+          class="mb-3 sm:col-span-6 col-span-12"
         />
 
         <Select
@@ -64,7 +75,7 @@ watch(() => products_request.storage_id, async (storage_id) => { await fill({ st
           :options="statusOptions"
           :required="true"
           v-model="products_request.status"
-          class="m-3 sm:col-span-6 col-span-12"
+          class="mb-3 sm:col-span-6 col-span-12"
         />
 
         <!--
@@ -72,7 +83,7 @@ watch(() => products_request.storage_id, async (storage_id) => { await fill({ st
             label="Комментарий"
             :required="true"
             v-model="products_request.comment"
-            class="m-3 sm:col-span-6 col-span-12"
+            class="mb-3 sm:col-span-6 col-span-12"
           />
         -->
 
